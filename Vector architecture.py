15773a5612b1cb050c7b08707e506a98e9573fbc
shassan30743 <shassan30743@ucumberlands.edu>
2024-11-03 21:40:02 -0500

    Create Vector architecture.py

---
+++ Vector architecture.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Initialize two vectors for addition
+vector1 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+vector2 = np.array([8, 7, 6, 5, 4, 3, 2, 1])
+
+# Perform vector addition
+result = np.add(vector1, vector2)
+
+print("Vector 1:", vector1)
+print("Vector 2:", vector2)
+print("Result of vector addition:", result)
